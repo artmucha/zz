6b9eee69f6e9e1c880ca1caa41a969c35a8ec2b3
Artur Mucha <arti.mucha@gmail.com>
2021-11-16 15:28:06 +0100

    add filter routes

--- src/router/filters.ts
+++ src/router/filters.ts
@@ -5,11 +5,9 @@ const filterRouter = express.Router();
 
 filterRouter.get('/api/filters', (req: Request, res: Response) => {
     const search = req.query.search;
-    console.log(search)
     //@ts-ignore
     const searchedCitiies = cities.filter(city => city.Name.toLowerCase().startsWith(search.toLowerCase()));
-    console.log(searchedCitiies)
-    res.send(searchedCitiies);
+    res.status(200).send(searchedCitiies);
 });
 
 export { filterRouter };
