d9b3857ac5bc370e5f21b88e5ecee8d3bc70f72d
Artur Mucha <arti.mucha@gmail.com>
2021-11-22 18:13:50 +0100

    add posts router

--- src/router/posts.ts
+++ src/router/posts.ts
@@ -7,19 +7,35 @@ import { validateRequest } from '../middlewares/validateRequest';
 import { requireAuth } from '../middlewares/requireAuth';
 import { NotFoundError } from '../errors/notFoundError';
 import { NotAuthorizedError } from '../errors/notAuthorizedError';
+import { createFile } from '../utils/helpers';
 
 const postRouter = express.Router();
 
 postRouter.get('/api/posts', async (req: Request, res: Response) => {
-	const type = req.params.type ? { 'type' : req.params.type } : {};
-	const category = req.params.category ? { 'category': req.params.category } : {};
+	const type = req.query.type ? { 'type' : req.query.type } : {};
+	const category = req.query.category ? { 'category': req.query.category } : {};
 	const location = req.query.location ? { 'location': req.query.location } : {};
 	const price = req.query.price ? { 'price': req.query.price } : {};
+	const page = req.query.page;
+
+	//@ts-ignore
+	const currentPage:number = page || 1;
+  	const perPage:number = 20;
 
 	try {
 		//@ts-ignore
-		const posts = await Post.find({...type, ...category, ...location, ...price}).sort({createdAt: 'desc'});
-		res.status(200).send(posts);
+		const posts = await Post.find({...type, ...category, ...location, ...price})
+			.skip((currentPage - 1) * perPage)
+			.limit(perPage)
+			.sort({createdAt: 'desc'});
+
+		//@ts-ignore
+		const totalPosts = await Post.find({...type, ...category, ...location, ...price}).countDocuments();
+		res.status(200).send({
+			posts,
+			currentPage: Number(currentPage),
+          	maxPage: Math.ceil(totalPosts / perPage)
+		});
 	  } catch (error) {
 		throw new NotFoundError();
 	}
@@ -62,7 +78,15 @@ postRouter.post('/api/posts',
     ], 
     validateRequest,
     async (req: Request, res: Response) => {
-        const { title, description, location, category, price, type, date } = req.body;
+        const { title, description, location, category, type, date, slug, image, price } = req.body;
+
+		let url = '';
+
+		if (image !== '') {
+			url = createFile(image, slug);
+		} else {
+			url = `http://localhost:5000/upload/media/posts/${category}.jpg`;
+		}
 
         const post = new Post({ 
 			title, 
@@ -70,9 +94,11 @@ postRouter.post('/api/posts',
 			location, 
 			category,
 			type,
-			price, 
 			date,
-			userId: req.currentUser!._id
+			slug,
+			price, 
+			image: url,
+			// userId: req.currentUser!._id
 		});
 
 		await post.save();
@@ -107,7 +133,7 @@ postRouter.put('/api/posts/:slug',
     validateRequest,
 	async (req: Request, res: Response) => {
 	const slug = req.params.slug ? { 'slug' : req.params.slug } : {};
-	const { title, description, location, category, price, type, date } = req.body;
+	const { title, description, location, category, price, type } = req.body;
 
 	const post = await Post.findOne(slug);
 
@@ -123,7 +149,7 @@ postRouter.put('/api/posts/:slug',
 		location, 
 		category,
 		type,
-		price,
+		price
 	});
 
 	await post.save();
